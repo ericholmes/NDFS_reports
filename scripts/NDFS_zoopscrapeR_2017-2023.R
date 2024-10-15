2a967acff0e55e3f790ac6d26f50537267b742a6
ericholmes <ejholmes@ucdavis.edu>
2024-10-15 15:19:10 -0700

    Expanded code to include 2016 datasheets

--- scripts/NDFS_zoopscrapeR_2017-2023.R
+++ scripts/NDFS_zoopscrapeR_2017-2023.R
@@ -7,10 +7,11 @@ library(tidyverse)
 files <- list.files("data/zoopraw", pattern = "xlsx")
 
 #Create empty zoop index data frame
-index_zoop <-data.frame()
-
+index_zoop <- data.frame()
+# file = "20160907 YB Zooplankton ID Data Sheet - SAMPLES 33-51 - EXPEDITED.xlsx"
 for(file in files){
   sheets <- readxl::excel_sheets(paste0("data/zoopraw/", file))
+  # i = "I80 42"
   sheets <- sheets[grepl(sheets, pattern = " ") & !grepl(sheets, pattern = "COC") & 
                      !grepl(sheets, pattern = "RPD") & !grepl(sheets, pattern = "QAQC") &
                               !grepl(sheets, pattern = "DUP")]
@@ -28,7 +29,7 @@ for(file in files){
     clad_start_index <- grep("CLADOCERA", tempdat[,17])
     harp_start_index <- grep("HARPACTICOIDS", tempdat[,17])
     macro_start_index <- grep("MACROZOOPLANKTON", tempdat[,17])
-    grep("Unable", tempdat[,18])
+    macro_end_index <- grep("unable", tolower(tempdat[,18]))
     
     index_zoop <- rbind(index_zoop, data.frame(
       file = file,
@@ -46,8 +47,10 @@ for(file in files){
       clad_end = harp_start_index - 1,
       harp_start = harp_start_index, harp_end = harp_start_index,
       macro_start = macro_start_index,
-      macro_end = grep("unable", tolower(tempdat[,18])) - 1)
-      )
+      macro_end = ifelse(length(macro_end_index) == 0,  
+                         which(is.na(vec_1[cal_start_index:length(vec_1)]))[1] + cal_start_index - 2, 
+                         macro_end_index - 1)
+      ))
     
     rm(tempdat, tdf, i)
   }
@@ -113,20 +116,28 @@ for(file in files){
       
       ##Gather volumes from sample processing. Using sum(VOLUME CELLS, na.rm = T) fixes 
       ##location issue of data being entered inconsistently across several cells
-      tdf$vol_meso <- sum(as.numeric(tempdat[grep("V1", tempdat[,11]), 13:17]), na.rm = T)
-      tdf$vol_micro <- sum(as.numeric(tempdat[grep("V2", tempdat[,1]), c(3,4)]), na.rm = T)
-      tdf$sub_meso <- sum(as.numeric(tempdat[grep("Sub1", tempdat[,19]), 21:23]), na.rm = T)
-      
+      tdf$vol_meso <- ifelse(is.na(as.numeric(sub(".*: ", "", tempdat[grep("V1", tempdat[,11]),11]))),
+             sum(as.numeric(tempdat[grep("V1", tempdat[,11]), 13:17]), na.rm = T),
+             as.numeric(sub(".*: ", "", tempdat[grep("V1", tempdat[,11]),11])))
+      tdf$vol_micro <- ifelse(is.na(as.numeric(sub(".*: ", "", tempdat[grep("V2", tempdat[,1]),1]))),
+                              sum(as.numeric(tempdat[grep("V2", tempdat[,1]), c(3,4)]), na.rm = T),
+                              as.numeric(sub(".*: ", "", tempdat[grep("V2", tempdat[,1]),1])))
+      tdf$sub_meso <- ifelse(is.na(as.numeric(sub(".*: ", "", tempdat[grep("Sub1", tempdat[,19]),19]))),
+                              sum(as.numeric(tempdat[grep("Sub1", tempdat[,19]), 21:23]), na.rm = T),
+                              as.numeric(sub(".*: ", "", tempdat[grep("Sub1", tempdat[,19]),19])))
       ##Solve problem of no data for micro volume: if length zero, then replace with NA
-      tdf$sub_micro <- ifelse(length(sum(as.numeric(tempdat[grep("Sub2", tempdat[,6]), c(8,9)]), na.rm = T)) > 0,
-                              sum(as.numeric(tempdat[grep("Sub2", tempdat[,6]),  c(8,9)]), na.rm = T), NA)
+      tdf$sub_micro <- ifelse(is.na(as.numeric(sub(".*: ", "", tempdat[grep("Sub2", tempdat[,6]),6]))),
+                              ifelse(length(sum(as.numeric(tempdat[grep("Sub2", tempdat[,6]), c(8,9)]), na.rm = T)) > 0,
+                                     sum(as.numeric(tempdat[grep("Sub2", tempdat[,6]),  c(8,9)]), na.rm = T), NA),
+                             as.numeric(sub(".*: ", "", tempdat[grep("Sub2", tempdat[,6]),6])))
       
       ##Solve ICF sheet artifact: if micro volume is zero, then use macro volume instead
       tdf$vol_micro <- ifelse(tdf$vol_micro == 0, tdf$vol_meso, tdf$vol_micro)
       tdf$sub_micro <- ifelse(tdf$sub_micro == 0, tdf$sub_meso, tdf$sub_micro)
-      
-      tdf$net_mesh <- ifelse(tolower(tempdat[grep("150um", tempdat[,10]),9]) == "x", "150", "50")
-      
+      tdf$net_mesh <- ifelse(length(grep("150", tempdat[grep("Zoop net type = ", tempdat[,1]),1])) == 1, "150",
+                             ifelse(length(grep(" 50", tempdat[grep("Zoop net type = ", tempdat[,1]),1])) == 1, "50",
+                                    ifelse(tolower(tempdat[grep("150um", tempdat[,10]),9]) == "x", "150", "50")))
+
       zoopdat_raw <- rbind(zoopdat_raw, tdf[is.na(tdf$count) == F, ])
     }
   }
@@ -139,6 +150,14 @@ zoopdat_raw$sample_date <- as.Date(zoopdat_raw$sample_date, format = "%Y%m%d")
 zoopdat <- zoopdat_raw
 zoopdat <- zoopdat[is.na(zoopdat$count) == F, ]
 
+###FIX DATA ERRORS
+##Fix name transcription errors
+zoopdat$station_id <- ifelse(zoopdat$station_id == "I-80", "I80", 
+                          ifelse(zoopdat$station_id == "RCs", "RCS",
+                                 ifelse(zoopdat$station_id %in% c("RIO VISTA", "RV"), "RVB", zoopdat$station_id)))
+##Fix date transcription error (datasheet exists for STTD on 2017-9-20, not 2017-9-27)
+zoopdat[zoopdat$station_id == "STTD" & zoopdat$sample_date == as.Date("2017-9-27"), "sample_date"] <- as.Date("2017-9-20")
+
 ## Optional: export unique critters for developing lookup table
 critters <- zoopdat[duplicated(zoopdat$taxon) == F, c("taxon", "classification")]
 # write.csv(critters, "data/critters.csv", row.names = F)
@@ -148,6 +167,8 @@ zooplookup <- read.csv("C:/Users/eholmes/Documents/R/Projects/NDFS-Projects/IEP_
 unique(zoopdat$taxon) %in% zooplookup$Organism
 glimpse(zoopdat)
 unique(paste(zoopdat[is.na(zoopdat$net_mesh), "sample_date"], zoopdat[is.na(zoopdat$net_mesh), "station_id"]))
+
+zoopdat2018 <- zoopdat[zoopdat$sample_date < as.Date("2019-01-01") & zoopdat$sample_date > as.Date("2018-01-01"),]
 
 # Add rotations data ------------------------------------------------------
 
@@ -182,17 +203,21 @@ colnames(histrots) <-c("PhysicalDataID", "sampling_event_date", "sampling_area_n
 
 histrots$sampling_event_date <- as.Date(histrots$sampling_event_date, format = "%m/%d/%Y")
 
-##Combine lower trophic metadata and historical roations data
+##Remove duplicate entry for RCS on 2019-9-17, confirmed correct rotations on LT datasheet
+histrots <- histrots[!(histrots$PhysicalDataID == 1944),]
+
+##Combine lower trophic metadata and historical rotations data
 ltdfull <- rbind(ltd[,c("sampling_event_date", "sampling_area_number", "flow_meter_start_150", 
                         "flow_meter_end_150", "flow_meter_speed", "field_comments")], 
                  histrots[,c("sampling_event_date", "sampling_area_number", "flow_meter_start_150", 
                              "flow_meter_end_150", "flow_meter_speed", "field_comments")])
+ltdfull$flow_meter_speed <- ifelse(is.na(ltdfull$flow_meter_speed) == T, "Low", ltdfull$flow_meter_speed)
 
 ##Merge rotations data with zoop raw data
 zoop <- merge(zoopdat[zoopdat$net_mesh == "150",], ltdfull, 
               by.x = c("station_id", "sample_date"), by.y = c("sampling_area_number", "sampling_event_date"), all.x = T)
 
-ltdrvb <- ltd[ltd$sampling_area_number == "RVB" & ltd$sampling_event_date == as.Date("2023-09-06"),]
+ltdrvb <- ltd[ltd$sampling_area_number == "RVB" & ltd$sampling_event_date == as.Date("2016-07-07"),]
 
 zoop <- zoop[is.na(zoop$count) == F & 
                zoop$station_id %in% c("BL5", "I-80", "I80", "LIB", "LIS",  
@@ -219,10 +244,17 @@ zoop$CPUE <-  round((zoop$count/zoop$sampfraction)/zoop$volume,3)
 ggplot(zoop, aes(x = CPUE)) + geom_histogram() + scale_x_log10()
 ggplot(zoop, aes(x = CPUE)) + geom_histogram() + scale_x_log10() + facet_wrap(station_id ~ .)
 
+ggplot(zoop, aes(x = sampfraction)) + geom_histogram()
+
+badsamps <- zoop[zoop$sampfraction >1,]
+
 zoop$jday <- as.integer(format(zoop$sample_date, format = "%j"))
 zoop$Week <- as.integer(format(zoop$sample_date, format = "%W"))
 zoop <- zoop[is.na(zoop$count) == F,]
 
+rvbbad <- zoop[(zoop$station_id %in% "RVB" & zoop$sample_date %in% as.Date("2016-07-07")),]
+zoop <- zoop[!(zoop$station_id %in% "RVB" & zoop$sample_date %in% as.Date("2016-07-07")),]
+
 if(readline(prompt = "Save data? (y/n): ") == "y"){
-  save(zoop, file = "data/NDFS2017-2023_zoop.Rdata")
+  save(zoop, file = "data/NDFS2016-2023_zoop.Rdata")
   write.csv(zoop, file = "data/NDFS2017-2023_zoop.csv", row.names = F)}
